256cb2665dfd547c43375b652291f08104cafdbc
Leo6485 <lo570354@gmail.com>
2025-02-15 17:06:11 -0300

    Enhance instruction handling by adding support for new I-type and J-type instructions; refactor opcode mapping for clarity

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from sys import argv
 import re
 
 d = """
-li $s1 10
-addi $s2 $s2 5
-add $s1 $s1 $s1
+inicio:
+addi $t0, $t0, 10
 j exit
+sw $t0, 4, $zero
+lw $ra, 4, $zero
 exit:
 """
 
@@ -32,7 +33,7 @@ def get_reg(reg):
     return format(int(num), '05b')
 
 def binstr_to_hexstr(num):
-    return hex(int(num, 2))[2:].zfill(8) # Garante que tenha 8 casa hexadecimais mesmo para números pequenos
+    return hex(int(num, 2))[2:].zfill(8)
 
 def R(inst):
     opcode = "000000"
@@ -42,7 +43,9 @@ def R(inst):
     shamt = "00000"
     
     funct_map = {
-        "add": "100000",
+        "add": "100000", "sub": "100010", "and": "100100", "or": "100101",
+        "xor": "100110", "nor": "100111", "slt": "101010", "sll": "000000",
+        "srl": "000010", "sra": "000011"
     }
     funct = funct_map[inst[0]]
     
@@ -50,44 +53,60 @@ def R(inst):
 
 def I(inst):
     opcode_map = {
-        "addi": "001000"
+        "addi": {"code": "001000", "rs": inst[2], "rt": inst[1], "imm": inst[3]},
+        "lw": {"code": "100011", "rs": inst[3], "rt": inst[1], "imm": inst[2]},
+        "sw": {"code": "101011", "rs": inst[3], "rt": inst[1], "imm": inst[2]},
+        "andi": {"code": "001100", "rs": inst[2], "rt": inst[1], "imm": inst[3]},
+        "ori": {"code": "001101", "rs": inst[2], "rt": inst[1], "imm": inst[3]},
+        "xori": {"code": "001110", "rs": inst[2], "rt": inst[1], "imm": inst[3]},
+        "slti": {"code": "001010", "rs": inst[2], "rt": inst[1], "imm": inst[3]},
+        "beq": {"code": "000100", "rs": inst[1], "rt": inst[2], "imm": inst[3]},
+        "bne": {"code": "000101", "rs": inst[1], "rt": inst[2], "imm": inst[3]}
     }
     
-    # Pseudo instrução
     if inst[0] == "li":
-        return I(["addi", inst[1], "$0", inst[2]])
+        inst = {"code": "001000", "rs": inst[1], "rt": "$0", "imm": inst[2]}
+    else:
+        inst = opcode_map[inst[0]]
     
-    opcode = opcode_map[inst[0]]
-    rs = get_reg(inst[2])
-    rt = get_reg(inst[1])
-    imm = i2bin(inst[3])
+    opcode = inst["code"]
+    rs = get_reg(inst["rs"])
+    rt = get_reg(inst["rt"])
+    imm = i2bin(inst["imm"])
     
     return opcode + rs + rt + imm
 
-def J(inst):
-    opcode = "000010"
-    
-    addr = format(0, '026b')
-    
+def J(inst, labels):
+    opcode_map = {
+        "j": "000010", "jal": "000011"
+    }
+    opcode = opcode_map[inst[0]]
+    addr = format(labels[inst[1]], '026b')
+
     return opcode + addr
+
+data = data.replace(",", "")
+data_labels = data.replace(":\n", ":")
+data_labels = data_labels.split("\n")
 
 data = [i.split() for i in data.strip().split("\n")]
 
 binary_instructions = []
 labels = {}
-for i, line in enumerate(data):
-    if line[0] in ["addi", "li"]:
-        binary_instructions.append(I(line))
-    elif line[0] in ["add"]:
-        binary_instructions.append(R(line))
-    elif line[0] in ["j"]:
-        binary_instructions.append(J(line))
-    elif line[-1][-1] == ":":
-        labels[line[-1][:-1]] = i
-    
-    print(line)
-    print(binary_instructions[-1])
 
-print(labels)
+for i, line in enumerate(data_labels):
+    if ":" in line:
+        pos = line.index(":")
+        labels[line[:pos]] = i - 1
+del data_labels
+
+for i, line in enumerate(data):
+    if line[0] in ["addi", "li", "lw", "sw", "andi", "ori", "xori", "slti", "beq", "bne"]:
+        binary_instructions.append(I(line))
+    elif line[0] in ["add", "sub", "and", "or", "xor", "nor", "slt", "sll", "srl", "sra"]:
+        binary_instructions.append(R(line))
+    elif line[0] in ["j", "jal"]:
+        binary_instructions.append(J(line, labels))
+
 for bin_inst in binary_instructions:
     print(binstr_to_hexstr(bin_inst))
